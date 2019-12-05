1c64fd9d0b60a05c12325dc660b81630b417cc99
ansarimofid <ansarimofid@gmail.com>
2019-12-05 16:15:02 +0530

    Adds style to tag page

--- src/pages/tags/index.js
+++ src/pages/tags/index.js
@@ -12,31 +12,24 @@ const TagsPage = ({
     },
   },
 }) => (
-  <Layout>
-    <section className="section">
-      <Helmet title={`Tags | ${title}`} />
-      <div className="container content">
-        <div className="columns">
-          <div
-            className="column is-10 is-offset-1"
-            style={{ marginBottom: '6rem' }}
-          >
-            <h1 className="title is-size-2 is-bold-light">Tags</h1>
-            <ul className="taglist">
-              {group.map(tag => (
-                <li key={tag.fieldValue}>
-                  <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
-                    {tag.fieldValue} ({tag.totalCount})
+    <Layout>
+      <section className="section flex">
+        <Helmet title={`Tags | ${title}`} />
+        <div className="max-w-4xl mr-auto ml-auto mt-20 mb-20">
+          <h1 className="text-5xl font-bold italic text-black text-center">All Tags</h1>
+          <ul className="flex flex-wrap mt-10 justify-center">
+            {group.map(tag => (
+              <li key={tag.fieldValue}>
+                <Link className="text-gray-87 rounded mr-2 ml-2 inline-block bg-gray-245 px-4 py-2 mt-4" to={`/tags/${kebabCase(tag.fieldValue)}/`}>
+                  #{tag.fieldValue} ({tag.totalCount})
                   </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
+              </li>
+            ))}
+          </ul>
         </div>
-      </div>
-    </section>
-  </Layout>
-)
+      </section>
+    </Layout>
+  )
 
 export default TagsPage
 
