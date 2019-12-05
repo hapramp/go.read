c7a3896bfe8e99d2ad8d71a5c0ccd8181d0e42dc
ansarimofid <ansarimofid@gmail.com>
2019-12-05 21:11:13 +0530

    Minor updates

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
 import BlogRoll from '../components/BlogRoll'
+
+
+import contestIcon from '../img/contest.svg';
+import challengesIcon from '../img/challenges.svg';
+import blogIcon from '../img/blog.svg';
+import announcementIcon from '../img/announcement.svg';
 
 export const IndexPageTemplate = ({
   image,
@@ -16,27 +23,47 @@ export const IndexPageTemplate = ({
 }) => (
     <div>
       <div
-        className=""
+        className="bg-center bg-no-repeat bg-secondary bg-cover"
         style={{
           backgroundImage: `url(${
             !!image.childImageSharp ? image.childImageSharp.fluid.src : image
             })`,
         }}
       >
-        <div className="max-w-4xl mx-auto text-white text-center">
-          <div className="p-10"></div>
-          <h1 className="text-5xl font-black italic text-white"> {title}</h1>
-          <h5 className="text-lg">{subheading}</h5>
-          <div className="p-10"></div>
+        <div className="max-w-4xl mx-auto text-white text-center py-10 sm:py-20">
+          <h1 className="text-2xl sm:text-4xl md:text-5xl font-black sm:font-black italic text-white"> {title}</h1>
+          <h5 className="mt-2 text-base sm:text-base md:text-lg px-4 leading-tight">{subheading}</h5>
         </div>
       </div>
       <section className="">
-        <div className="container ml-auto mr-auto">
-          <div className="mt-10">
-            <h3 className="text-2xl uppercase text-primary font-bold pl-4 pr-4">Latest</h3>
-            <div className="mt-10"><BlogRoll /></div>
+      <div className="container mr-auto ml-auto">
+            <div className="sm:mt-10">
+              <div className="flex h-20 sm:hidden bg-gray-245 py-4 text-gray-70 w-screen overflow-y-hidden">
+                <div className="flex h-24 overflow-x-auto">
+                  <Link className="font-black italic md:text-lg lg:text-xl flex flex-shrink-0 items-center mx-2 bg-white px-4 py-2 rounded h-12 ml-4" to="/tags/contest/">
+                    <img className="mr-2" src={contestIcon} alt="" />
+                    Contest
+                  </Link>
+                  <Link className="font-black italic md:text-lg lg:text-xl flex flex-shrink-0 items-center mx-2 bg-white px-4 py-2 rounded h-12" to="/tags/challenges/">
+                    <img className="mr-2" src={challengesIcon} alt="" />
+                    Challenges
+                  </Link>
+                  <Link className="font-black italic md:text-lg lg:text-xl flex flex-shrink-0 items-center mx-2 bg-white px-4 py-2 rounded h-12" to="/tags/general/">
+                    <img className="mr-2" src={blogIcon} alt="" />
+                    General
+                  </Link>
+                  <Link className="font-black italic md:text-lg lg:text-xl flex flex-shrink-0 items-center mx-2 bg-white px-4 py-2 rounded h-12" to="/tags/announcement">
+                    <img className="mr-2" src={announcementIcon} alt="" />
+                    Announcement
+                  </Link>
+                  <div className="p-1"></div>
+                </div>
+                
+              </div>
+              <h3 className="mt-5 sm:mt-0 text-base sm:text-2xl uppercase text-primary font-bold pl-4 pr-4">Latest</h3>
+              <div className="mt-5 sm:mt-10"><BlogRoll /></div>
+            </div>
           </div>
-        </div>
       </section>
     </div>
   )
