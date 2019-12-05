1db6d65cad748790049f0786547113d77dd70db6
ansarimofid <ansarimofid@gmail.com>
2019-12-05 16:45:20 +0530

    Adds error 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { Link } from 'gatsby'
 
 const NotFoundPage = () => (
-  <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+  <Layout className="h-full">
+    <section>
+      <div className="max-w-4xl ml-auto mr-auto text-center mt-20 mb-20">
+        <h1 className="text-5xl text-gray-87">NOT FOUND :(</h1>
+        <p className=" text-gray-70 mt-4">This link is broken, looks like a <span className="italic">challenge</span> for us.</p>
+        <div className="mt-20 text-xl ">
+          For now, Go back to <Link className="text-secondary uppercase" to="/">Home</Link>
+        </div>
+      </div>
+    </section>
+
   </Layout>
 )
 
