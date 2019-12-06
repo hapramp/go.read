3ce6223dc6b4eed4d0b268f36329c6c49e6c76d2
ansarimofid <ansarimofid@gmail.com>
2019-12-06 13:48:39 +0530

    Adds privacy and terms page and also adds style in homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Home from '../components/Home';
 import { Helmet } from 'react-helmet'
-import { withPrefix } from 'gatsby'
+import { withPrefix } from 'gatsby';
+import '../styles/global.css';
 
 const Index = () => {
   return (
