ee251553a4a841d394656ebb735c478dbc0ca23b
ansarimofid <ansarimofid@gmail.com>
2020-02-11 11:58:26 +0530

    Updates directory of config file

--- src/components/Home/sections/CTA.js
+++ src/components/Home/sections/CTA.js
@@ -15,7 +15,7 @@ const CTA = () => {
         <p className="text-xl md:text-2xl mt-4">We believe the most important thing for creators is to never stop creating.</p>
         <div className=" mt-16">
           <a href="https://play.google.com/store/apps/details?id=com.go.social.prod&referrer=utm_source%3Dgetgosocial.app%26utm_medium%3Dbutton_cta%26utm_term%3Dget%252Bit%252Bon%252Bplaystore%26utm_campaign%3Dlanding_page" className="text-white shadow sm:shadow-none mt-4 md:mt-0 flex-grow bg-primary py-4 px-6 rounded cursor-pointer" target="__new">
-          <svg className="inline mr-2" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd">
+          <svg className="inline mr-2" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd">
             <path fill="#fff" d="M1.571 23.664l10.531-10.501 3.712 3.701-12.519 6.941a1.563 1.563 0 01-1.532-.011l-.192-.13zm9.469-11.56L1 22.115V2.093l10.04 10.011zm6.274-4.137l4.905 2.719c.482.268.781.77.781 1.314s-.299 1.046-.781 1.314l-5.039 2.793-4.015-4.003 4.149-4.137zM1.46.433A1.563 1.563 0 013.295.195L15.948 7.21l-3.846 3.835L1.46.433z"/>
           </svg>
           <span>Get it on Playstore</span>
--- src/pages/saythanks/index.js
+++ src/pages/saythanks/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet'
 import { withPrefix } from 'gatsby';
 
-import { getFirebase } from './config/fbConfig';
+import { getFirebase } from '../../config/fbConfig';
 
 const User = (props) => {
 
